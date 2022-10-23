a0d70cbb98ba155170494bd8c5b52225d841149a
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-22 21:39:39 -0400

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -6,9 +6,6 @@ using StringTools;
 
 class HealthIcon extends FlxSprite
 {
-	/**
-	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
-	 */
 	public var sprTracker:FlxSprite;
 
 	public var char:String;
