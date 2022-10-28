bc6ce38503915331581cc01b04e6bd4139a8b3c0
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-27 23:48:13 -0400

    maybe this should be &&

--- source/Note.hx
+++ source/Note.hx
@@ -33,7 +33,7 @@ class Note extends FlxSprite
 	
 	inline function get_canBeHit()
 	{
-		return strumTime > Conductor.songPosition - Conductor.safeZoneOffset - strumTime < Conductor.songPosition + 0.5 * Conductor.safeZoneOffset;
+		return strumTime > Conductor.songPosition - Conductor.safeZoneOffset && strumTime < Conductor.songPosition + 0.5 * Conductor.safeZoneOffset;
 	}
 
 	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false)
