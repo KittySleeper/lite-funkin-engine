62f1cf785729b22837d0e90d70e3f56b62494585
team-504 <75187806+team-504@users.noreply.github.com>
2022-11-21 14:16:07 -0600

    custom discord real

--- source/Discord.hx
+++ source/Discord.hx
@@ -11,7 +11,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "814588678700924999",
+			clientID: "1044343228457046036",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
